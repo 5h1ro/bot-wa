b0aa6d5fc68c473b90050734c2ebe94bb3fe913f
5h1ro <nurhakiki.ri@gmail.com>
2024-02-11 20:19:43 +0700

    fix: nbug

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const client = new Client({
         args: [
             "--no-sandbox",
         ],
-        browserWSEndpoint: "ws://localhost:9337/?--user-data-dir=/usr/src/app/user-data-dir/ioniabotbrowser",
+        browserWSEndpoint: "ws://localhost:9337/",
     },
 });
 
