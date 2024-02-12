6162112d0231f5496e998b6f5b193f0453775124
5h1ro <nurhakiki.ri@gmail.com>
2024-02-12 09:16:21 +0700

    fix: add gojo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from characterai import PyCAI
+from characterai import PyAsyncCAI
 
 app = FastAPI()
 class Item(BaseModel):
@@ -25,17 +26,17 @@ async def zero_two(item: Item):
 
 @app.post("/gojo")
 async def gojo(item: Item):
-    client = PyCAI('a223eb3dca256d3b14dd6df5b2b4ece49f281b61')
+    client = PyAsyncCAI('a223eb3dca256d3b14dd6df5b2b4ece49f281b61')
     char = '4WOVrCApi4JYwfYwU2e5eDeFalLOkGBw6IfUZPX1XVQ'
-    chat = client.chat.get_chat(char)
-    participants = chat['participants']
-    if not participants[0]['is_human']:
-        tgt = participants[0]['user']['username']
-    else:
-        tgt = participants[1]['user']['username']
-        
-    data = client.chat.send_message(
-        chat['external_id'], tgt, item.message
-    )
-    text = data['replies'][0]['text']
+    chat = await client.chat2.get_chat(char)
+    author = {
+        'author_id': chat['chats'][0]['creator_id']
+    }
+    async with client.connect() as chat2:
+        data = await chat2.send_message(
+            char, chat['chats'][0]['chat_id'], 
+            item.message, author
+        )
+    
+    text = data['turn']['candidates'][0]['raw_content']
     return text
