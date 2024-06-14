239111c725aa53a8d71b65cc5c99c0dbe6e40492
Nurhakiki Romadhony Ikhwandany <68720292+5h1ro@users.noreply.github.com>
2024-06-14 20:44:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Item(BaseModel):
 
 @app.post("/")
 async def freya(item: Item):
-    client = PyAsyncCAI('a223eb3dca256d3b14dd6df5b2b4ece49f281b61')
+    client = PyAsyncCAI('744de7c2f7fa65d11bee2c72be38c7a1cecb7662')
     char = 'HG08QzTxNtPelQR0w3wTOm_crRtfhgQdbJuESv0vbgs'
     chat = await client.chat2.get_chat(char)
     author = {
@@ -25,7 +25,7 @@ async def freya(item: Item):
 
 @app.post("/gojo")
 async def gojo(item: Item):
-    client = PyAsyncCAI('a223eb3dca256d3b14dd6df5b2b4ece49f281b61')
+    client = PyAsyncCAI('744de7c2f7fa65d11bee2c72be38c7a1cecb7662')
     char = '4WOVrCApi4JYwfYwU2e5eDeFalLOkGBw6IfUZPX1XVQ'
     chat = await client.chat2.get_chat(char)
     author = {
@@ -42,7 +42,7 @@ async def gojo(item: Item):
 
 @app.post("/kobo")
 async def gojo(item: Item):
-    client = PyAsyncCAI('a223eb3dca256d3b14dd6df5b2b4ece49f281b61')
+    client = PyAsyncCAI('744de7c2f7fa65d11bee2c72be38c7a1cecb7662')
     char = '1UWABpuo0uP9Vn-ucD7CdrD8fNBq36KVpdVoBCzsSvM'
     chat = await client.chat2.get_chat(char)
     author = {
